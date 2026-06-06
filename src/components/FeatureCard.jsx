5f8ce30cf2a92ebd087cdc6bedf156388567ec7d
harshith_vinn <harshith.xg.11@gmail.com>
2026-06-06 11:23:31 +0530

    Complete landing page UI with animations and footer

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -1,5 +1,7 @@
 import "../styles/FeatureCard.css";
+// imported the css file for styling the feature card component
 function FeatureCard(props) {
+  // this component simply takes the props and displayes the title and description of the feature card and we can reuse the component for different features by passing different props to it
   return (
     <div className="feature-card">
       <h2>{props.title}</h2>
